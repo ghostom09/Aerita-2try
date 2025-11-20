f42f2e14594a545f71ae22f44f1e034aa1661193
ghostom09 <ghostom0612@gmail.com>
2025-11-20 16:18:49 +0900

    WIP
    settingUi move - fin

--- Assets/Scripts/MainMenu/ButtonManager.cs
+++ Assets/Scripts/MainMenu/ButtonManager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class ButtonManager : MonoBehaviour
 {
+    [SerializeField] private MainMenuUIManager uIManager;
     public void StartGame()
     {
         SceneManagement.instance.LoadScene(SceneName.Ghotom);
@@ -10,8 +11,32 @@ public class ButtonManager : MonoBehaviour
     {
         GameManager.instance.QuitGame();
     }
+    
     public void Setting()
     {
-        
+        uIManager.UI(UIName.SettingUI);
+    }
+
+    public void Graphic()
+    {
+        uIManager.Setting(SettingUI.Graphic);
+    }
+    public void Sound()
+    {
+        uIManager.Setting(SettingUI.Sound);
+    }
+    public void Keyboard()
+    {
+        uIManager.Setting(SettingUI.Keyboard);
+    }
+    public void Game()
+    {
+        uIManager.Setting(SettingUI.Game);
+    }
+
+    public void QuitSetting()
+    {
+        uIManager.SettingQuit();
+        uIManager.MainMenu();
     }
 }
---
+++ Assets/Scripts/MainMenu/MainMenuUIManager.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public enum UIName
+{
+    SettingUI,
+    SaveUI,
+}
+
+public enum SettingUI
+{
+    Graphic,
+    Sound,
+    Game,
+    Keyboard
+}
+public class MainMenuUIManager : MonoBehaviour
+{
+    [Header("UI")]
+    [SerializeField] private GameObject settingUI;
+    [SerializeField] private GameObject saveUI;
+    
+    [Header("Settings")]
+    [SerializeField] private GameObject graphic;
+    [SerializeField] private GameObject sound;
+    [SerializeField] private GameObject keyboard;
+    [SerializeField] private GameObject game;
+    public void UI(UIName uiName)
+    {
+        switch (uiName)
+        {
+            case UIName.SettingUI:
+                MainMenu();
+                settingUI.SetActive(true);
+                break;
+            case UIName.SaveUI:
+                MainMenu();
+                saveUI.SetActive(true);
+                break;
+        }
+    }
+    public void Setting(SettingUI settingUIName)
+    {
+        switch (settingUIName)
+        {
+            case SettingUI.Graphic:
+                SettingQuit();
+                graphic.SetActive(true);
+                break;
+            case SettingUI.Sound:
+                SettingQuit();
+                sound.SetActive(true);
+                break;
+            case SettingUI.Game:
+                SettingQuit();
+                game.SetActive(true);
+                break;
+            case SettingUI.Keyboard:
+                SettingQuit();
+                keyboard.SetActive(true);
+                break;
+        }
+    }
+
+    public void SettingQuit()
+    {
+        graphic.SetActive(false);
+        keyboard.SetActive(false);
+        game.SetActive(false);
+        sound.SetActive(false);
+    }
+
+    public void MainMenu()
+    {
+        settingUI.SetActive(false);
+        saveUI.SetActive(false);
+    }
+}
