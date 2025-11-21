857c69d239e1f81d9d3ef7946ac5649ad0bc8d02
qkshshhd <ssambo33@gmail.com>
2025-11-21 20:24:55 +0900

    EnemyFSM
    Idle, Move

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,46 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public state stat;
+    public EnemyStateMachine stateMachine;
+    public Transform target;
+    public Rigidbody2D rb;
+    private void Awake()
+    {
+        stateMachine = new EnemyStateMachine();
+        stat.currentHp = stat.hp;
+    }
+    
+    void Start()
+    {
+        stateMachine.ChangeState(new IdleState(this)); // 처음 상태: 도움 요청
+    }
+    void Update()
+    {
+        stateMachine.Update();
+    }
+    
+    
+}
+
+[Serializable]
+public class state
+{
+    [Header("basic stat")] 
+    public float hp;
+    public float currentHp;
+    public float damage;
+    public float range;
+
+
+    [Header("movement stat")] 
+    public float moveSpeed;
+    public float jumpHeight;
+
+    [Header("other stat")]
+    public float perceiveRange;
+    public float ignoreRange;
+}
